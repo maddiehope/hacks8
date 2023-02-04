62d9fd9fea4eef992927bef0c6ce91c5d5f9ea5a
Maddie <mph63858@uga.edu>
2023-02-04 18:59:03 -0500

    syntax fixes

--- app.py
+++ app.py
@@ -28,12 +28,16 @@ def price():
     # tries to proceed how intended
     try: 
         # grabbing the variables from the form
-        chain_select = request.form.get("chain_select")
+        chain_select = request.form.get("chain-select")
         food_select1 = request.form.get("food-select1")
         food_select2 = request.form.get("food-select2")
         food_select3 = request.form.get("food-select3")
         food_select4 = request.form.get("food-select4")
         food_select5 = request.form.get("food-select5")
+
+        #testing #took out required
+        print(chain_select)
+        print(food_select1)
 
         # creating a list of all the food_select values 
         select_items = [food_select1, food_select2, food_select3, food_select4, food_select5]
